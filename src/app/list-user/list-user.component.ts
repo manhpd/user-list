88342c90aa203e5b7096b488f2044bcc0606033e
Manh Pham <manhpd.hut@gmail.com>
2019-07-15 18:27:53 +0800

    Update Table after create success

--- src/app/list-user/list-user.component.ts
+++ src/app/list-user/list-user.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject, EventEmitter, ViewChild } from '@angular/core';
 import { UserService } from '../user-service.service';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 import { FormBuilder } from '@angular/forms';
 import { Validators } from '@angular/forms';
+import { MatTable } from '@angular/material';
 
 export interface DialogData {
   animal: string;
@@ -23,6 +24,11 @@ export class ListUserComponent implements OnInit {
  
   displayedColumns: string[] = ['id', 'email', 'first_name', 'last_name', 'avatar', 'update', 'delete'];
   dataSource = [];
+
+  @ViewChild(MatTable, {
+    static: true
+  }) table: MatTable<any>;
+
   getUsers(): void {
      this.userService.getUsers()
       .subscribe(users => this.dataSource = users.data);
@@ -41,6 +47,18 @@ export class ListUserComponent implements OnInit {
       console.log('The dialog was closed');
       this.animal = result;
     });
+    if(dialogRef.componentInstance){
+      dialogRef.componentInstance.onComplete$.subscribe(res => {
+        if(this.dataSource){
+          this.dataSource = [
+            res,
+            ...this.dataSource            
+          ]
+
+          //this.table.renderRows();
+        }
+      });
+    }    
   }
 
 }
@@ -50,7 +68,7 @@ export class ListUserComponent implements OnInit {
   templateUrl: 'create-user-dialog.html',
 })
 export class CreateUserDialog {
-  
+  onComplete$ = new EventEmitter<any>();
   profileForm = this.fb.group({
     first_name: ['', Validators.required],
     last_name: ['', Validators.required],
@@ -69,8 +87,11 @@ export class CreateUserDialog {
   onSubmit() {
     console.warn(this.profileForm.value);
     this.userService.createUser(this.profileForm.value)
-    .subscribe(
-      // user => datasource.push(user)
+    .subscribe(res => {
+        this.dialogRef.close();
+        this.onComplete$.emit(res);
+        // user => datasource.push(user)      
+      }      
     );
   }
 
